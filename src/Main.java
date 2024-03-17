4da411d73393b40be3853c861e0300e2c430aaf1
AGA2020 <hasan9u@gmail.com>
2024-03-17 20:14:03 +0300

    Proje tamamlandı.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        double a, p, sonuc, yaricapd, acid;
+        double p, sonuc, yaricapd, acid;
         p = 3.14;
         Scanner yaricap = new Scanner(System.in);
         System.out.print("Yarıçap uzunluğunu giriniz(cm) : " );
